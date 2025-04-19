5f8de783c1c8706e8c1c0b39f3b18077f083a7b1
souzafcharles <souzach@outlook.com>
2025-04-19 15:25:29 -0300

    feat: implement entity classes and configure application.properties

---
+++ backendrentalcar/src/main/java/com/github/souzafcharles/rentalcar/entity/Brand.java
@@ -0,0 +1,29 @@
+package com.github.souzafcharles.rentalcar.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_brand")
+public class Brand implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Brand name is required.")
+    private String name;
+
+    private String description;
+
+}
---
+++ backendrentalcar/src/main/java/com/github/souzafcharles/rentalcar/entity/Car.java
@@ -0,0 +1,40 @@
+package com.github.souzafcharles.rentalcar.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_car")
+public class Car implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Name is required.")
+    private String name;
+
+    @NotBlank(message = "Model is required.")
+    private String model;
+
+    @ManyToOne
+    @NotNull(message = "Brand is required.")
+    private Brand brand;
+
+    @ManyToMany
+    @JoinTable(name = "car_accessory.")
+    private List<Accessory> accessories;
+
+}
