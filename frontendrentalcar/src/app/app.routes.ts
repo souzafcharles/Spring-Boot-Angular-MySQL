fbe254e6d45beb24eda1e764da3bac3080cdfc36
souzafcharles <souzach@outlook.com>
2025-05-04 20:09:53 -0300

    feat: add carlist component and rentalcar.svg icon

--- frontendrentalcar/src/app/app.routes.ts
+++ frontendrentalcar/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CarlistComponent } from './components/carlist/carlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"car", component: CarlistComponent}
+];
---
+++ frontendrentalcar/src/app/components/carlist/carlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './carlist.component.html',
+  styleUrl: './carlist.component.scss'
+})
+export class CarlistComponent {
+
+}
